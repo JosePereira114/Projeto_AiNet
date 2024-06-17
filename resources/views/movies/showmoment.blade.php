f8439245bddbabe773fdbe122a27ba1cd266970a
JosePereira114 <2211033@my.ipleiria.pt>
2024-06-17 15:53:58 +0100

    Update showmoment.blade.php

--- resources/views/movies/showmoment.blade.php
+++ resources/views/movies/showmoment.blade.php
@@ -4,11 +4,13 @@
 
 @section('main')
 @if(session('pdf-path'))
-    <div class="alert alert-success">
-        <a href="{{ route('cart.downloadPdf') }}" class="btn btn-primary"><span>CLICK HERE TO:</span> Download Receipt</a>
-    </div>
+<div class="alert alert-success">
+    <a href="{{ route('cart.downloadPdf') }}" class="btn btn-primary" style="background-color: #007bff; color: #fff; border-color: #007bff;">
+        <span>CLICK HERE TO:</span> Download Receipt
+    </a>
+</div>
 @endif
-    <div class="flex flex-col">
-        @each('movies.shared.cardmoment', $movies, 'movie')
-    </div>
+<div class="flex flex-col">
+    @each('movies.shared.cardmoment', $movies, 'movie')
+</div>
 @endsection
