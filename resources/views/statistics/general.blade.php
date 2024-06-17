62c5f34243abc3add42cf8445d1dd30299c08d1e
JosePereira114 <2211033@my.ipleiria.pt>
2024-06-17 16:06:02 +0100

    Update general.blade.php

--- resources/views/statistics/general.blade.php
+++ resources/views/statistics/general.blade.php
@@ -7,25 +7,41 @@
 
 <body class="bg-gray-100">
     <div class="container mx-auto p-6">
-        <div class="chart-container max-w-3xl mx-auto">
-            <canvas id="myChart"></canvas>
-        </div>
-
-        <div class="mt-6 flex justify-center">
-            <div class="bg-blue-200 text-blue-800 px-4 py-2 rounded-lg shadow-md mr-4">
+        <div class="bg-blue-200 text-blue-800 px-4 py-2 rounded-lg shadow-md mr-4">
                 <span class="font-semibold">Total Tickets Sold: {{ $totalTickets }}</span>
             </div>
-            <div class="bg-blue-200 text-blue-800 px-4 py-2 rounded-lg shadow-md mr-4">
+            <div class="bg-blue-200 text-blue-800 px-4 py-2 shadow-md mr-4">
                 <span class="font-semibold">Total Screenings: {{ $totalScreenings }}</span>
             </div>
-            @foreach ($genres_countmovies as $genre)
-            <span class="font-semibold">Numero de filmes por Género:</span>
-            <div class="bg-blue-200 text-blue-800 px-4 py-2 rounded-lg shadow-md mr-4">
-                <span class="font-semibold">{{ $genre->name }}:  {{ $genre->movies_count }}</span>
-            </div>
-            @endforeach 
-            
-            
+        <div class="mt-6 flex justify-center">
+            <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Género
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Número de Filmes
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach ($genres_countmovies as $genre)
+                    <tr>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm text-gray-900">{{ $genre->name }}</div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm text-gray-900">{{ $genre->movies_count }}</div>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+
+
+
         </div>
     </div>
 </body>
